6a3652346e16dbfeadf950a8666e3031630f9f6b
ShulgaVadim <stiffler8888@gmail.com>
2020-10-19 23:38:37 +0300

    add base page/test

---
+++ src/test/java/tests/base/BaseTest.java
@@ -0,0 +1,41 @@
+package tests.base;
+
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utils.CapabilitiesGenerator;
+
+import java.util.concurrent.TimeUnit;
+
+@Log4j2
+@Listeners(TestListener.class)
+public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(ITestContext context) {
+
+        try {
+            driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
+        } catch (SessionNotCreatedException ex) {
+            Assert.fail("Браузер не был открыт. Проверьте, что используется корректная версия драйвера");
+            log.fatal(ex.getLocalizedMessage());
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        context.setAttribute("driver", driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
