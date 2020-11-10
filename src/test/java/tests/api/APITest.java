9483a2d56e95eaf084656e126a7ceeca2c85f235
ShulgaVadim <stiffler8888@gmail.com>
2020-11-11 01:36:34 +0300

    edit APITest

--- src/test/java/tests/api/APITest.java
+++ src/test/java/tests/api/APITest.java
@@ -28,41 +28,41 @@ public class APITest {
 
     @Test
     public void createEditDeleteTestRunTest() {
-        int project_id = createAndGetIDProject();
-        Response responseRun = adapter.post(new File("src/test/resources/runs/run"), "add_run/" + project_id, 200);
-        int run_id = responseRun.jsonPath().getInt("id");
-        responseRun = adapter.post(new File("src/test/resources/runs/edit_run"), "update_run/" + run_id, 200);
+        int projectId = createAndGetIDProject();
+        Response responseRun = adapter.post(new File("src/test/resources/runs/run"), "add_run/" + projectId, 200);
+        int runId = responseRun.jsonPath().getInt("id");
+        responseRun = adapter.post(new File("src/test/resources/runs/edit_run"), "update_run/" + runId, 200);
         adapter.isEdited(responseRun, "name", "This is test run after updating");
-        adapter.delete("delete_run/" + run_id, 200);
-        adapter.isDeleted("get_run/" + run_id, 400, "Field :run is not a valid test run.");
-        adapter.delete("delete_project/" + project_id, 200);
+        adapter.delete("delete_run/" + runId, 200);
+        adapter.isDeleted("get_run/" + runId, 400, "Field :run is not a valid test run.");
+        adapter.delete("delete_project/" + projectId, 200);
     }
 
     @Test
     public void createEditDeleteMilestoneTest() {
-        int project_id = createAndGetIDProject();
-        Response responseMilestone = adapter.post(new File("src/test/resources/milestones/milestone"), "add_milestone/" + project_id, 200);
-        int milestone_id = responseMilestone.jsonPath().getInt("id");
-        responseMilestone = adapter.post(new File("src/test/resources/milestones/edit_milestone"), "update_milestone/" + milestone_id, 200);
+        int projectId = createAndGetIDProject();
+        Response responseMilestone = adapter.post(new File("src/test/resources/milestones/milestone"), "add_milestone/" + projectId, 200);
+        int milestoneId = responseMilestone.jsonPath().getInt("id");
+        responseMilestone = adapter.post(new File("src/test/resources/milestones/edit_milestone"), "update_milestone/" + milestoneId, 200);
         adapter.isEdited(responseMilestone, "name", "Release 2.0");
-        adapter.delete("delete_milestone/" + milestone_id, 200);
-        adapter.isDeleted("get_milestone/" + milestone_id, 400, "Field :milestone_id is not a valid milestone.");
-        adapter.delete("delete_project/" + project_id, 200);
+        adapter.delete("delete_milestone/" + milestoneId, 200);
+        adapter.isDeleted("get_milestone/" + milestoneId, 400, "Field :milestone_id is not a valid milestone.");
+        adapter.delete("delete_project/" + projectId, 200);
     }
 
     @Test
     public void createEditDeleteTestCaseTest() {
-        int project_id = createAndGetIDProject();
-        Response responseSection = adapter.post(new File("src/test/resources/section"), "add_section/" + project_id, 200);
-        int section_id = responseSection.jsonPath().getInt("id");
-        Response responseTestCase = adapter.post(new File("src/test/resources/testcases/testcase"), "add_case/" + section_id, 200);
-        int testcase_id = responseTestCase.jsonPath().getInt("id");
-        responseTestCase = adapter.post(new File("src/test/resources/testcases/edit_testcase"), "update_case/" + testcase_id, 200);
+        int projectId = createAndGetIDProject();
+        Response responseSection = adapter.post(new File("src/test/resources/section"), "add_section/" + projectId, 200);
+        int sectionId = responseSection.jsonPath().getInt("id");
+        Response responseTestCase = adapter.post(new File("src/test/resources/testcases/testcase"), "add_case/" + sectionId, 200);
+        int testcaseId = responseTestCase.jsonPath().getInt("id");
+        responseTestCase = adapter.post(new File("src/test/resources/testcases/edit_testcase"), "update_case/" + testcaseId, 200);
         adapter.isEdited(responseTestCase, "title", "This is test case after updating");
-        adapter.delete("delete_case/" + testcase_id, 200);
-        adapter.isDeleted("get_case/" + testcase_id, 400, "Field :case_id is not a valid test case.");
-        adapter.delete("delete_project/" + project_id, 200);
-        adapter.isDeleted("get_project/" + project_id, 400, "Field :project_id is not a valid or accessible project.");
+        adapter.delete("delete_case/" + testcaseId, 200);
+        adapter.isDeleted("get_case/" + testcaseId, 400, "Field :case_id is not a valid test case.");
+        adapter.delete("delete_project/" + projectId, 200);
+        adapter.isDeleted("get_project/" + projectId, 400, "Field :project_id is not a valid or accessible project.");
     }
 }
 
